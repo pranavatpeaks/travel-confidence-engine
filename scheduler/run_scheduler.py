35411993849314214faf7823ffa56970018bd264
Pranav Kothapalli <kothapallipranav90@gmail.com>
2026-06-15 17:34:16 +0530

    Testing scheduler for EC2 instance

--- scheduler/run_scheduler.py
+++ scheduler/run_scheduler.py
@@ -3,15 +3,22 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from core.orchestrator import collect_fares_once
 
 
+def scheduled_job():
+
+    print("Running collection cycle...")
+
+    collect_fares_once()
+
+    print("Collection cycle finished.")
+
+
 scheduler = BlockingScheduler()
 
-
 scheduler.add_job(
-    collect_fares_once,
+    scheduled_job,
     trigger="interval",
     minutes=1,
 )
-
 
 print(
     "Scheduler started "
